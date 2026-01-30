246eb4224731ca5792d9670d0f03112be2fb82e2
Bernardo Brotelli <b.brotelle@gmail.com>
2026-01-30 10:40:28 +0000

    Refactor Home component to use task objects , TaskList and Task component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,19 @@
-import { IoMdClose } from "react-icons/io";
-
 import { useState } from "react";
+import { v4 } from "uuid";
 
 import AddTask from "../components/AddTask";
+import TaskList from "../components/TaskList";
 
 function Home() {
-  const [tasksValues, setTasksValues] = useState<string[]>([]);
+  let tasksMock = [
+    {id: v4(), title: 'Take the dog for a walk', completed: false},
+    {id: v4(), title: 'Throw the trash away', completed: false},
+  ];
+
+  const [tasks, setTasks] = useState(tasksMock);
 
   const saveTask = (taskTitle: string) => {
-    setTasksValues([...tasksValues, taskTitle]);
+    setTasks([...tasks, {id: v4(), title: taskTitle, completed: false}]);
   }
 
   return (
@@ -17,24 +22,7 @@ function Home() {
         <div className="flex flex-col mt-16 w-[500px]">
           <h1 className="text-5xl font-bold subpixel-antialiased tracking-wider mb-6">TODO</h1>
           <AddTask onSaveTask={saveTask}/>
-          
-          <ul className="flex flex-col rounded-lg bg-white shadow-lg shadow-indigo-500/25">
-            <div className="flex px-2 py-2 justify-between text-gray-400 border-b border-b-gray-200 mx-2">
-              <div>items</div>
-              <div>filtros</div>
-              <div>clear completed</div>
-            </div>
-            <li className="flex items-center px-5 text-gray-500 border-b border-b-gray-200 py-4 group">
-              <input type="checkbox" className="mr-2 accent-purple-500/25" />
-              <span>Task 1</span>
-              <IoMdClose className="ml-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
-            </li>
-            <li className="flex items-center px-5 text-gray-500 border-b border-b-gray-200 py-4 group">
-              <input type="checkbox" className="mr-2 accent-purple-500/25" />
-              <span>Task 2</span>
-              <IoMdClose className="ml-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
-            </li>
-          </ul>
+          <TaskList tasks={tasks}/>
         </div>
       </div>
     </>
