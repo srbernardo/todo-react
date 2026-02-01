379dd813717e865c30c0757566305d28c096811a
Bernardo Brotelli <b.brotelle@gmail.com>
2026-02-01 08:32:50 +0000

    Add updateTask function to Home component and pass to TaskList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,13 +16,17 @@ function Home() {
     setTasks([...tasks, {id: v4(), title: taskTitle, completed: false}]);
   }
 
+  const updateTask = (task: {id: string, title: string, completed: boolean}) => {
+    setTasks(tasks.map((t) => t.id === task.id ? task : t));
+  }
+  
   return (
     <>
       <div className="flex justify-center">
         <div className="flex flex-col mt-16 w-[500px]">
           <h1 className="text-5xl font-bold subpixel-antialiased tracking-wider mb-6">TODO</h1>
           <AddTask onSaveTask={saveTask}/>
-          <TaskList tasks={tasks}/>
+          <TaskList tasks={tasks} onTaskChange={updateTask}/>
         </div>
       </div>
     </>
