192861ee8a8b0cbf99be9ba1a30b4e80b213f915
Bernardo Brotelli <b.brotelle@gmail.com>
2026-02-01 09:16:53 +0000

    Add task count display and clear completed button

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,20 +1,27 @@
 import Task from "./Task";
 
-function TaskList({tasks, onTaskChange}: {tasks: {id: string, title: string, completed: boolean}[], onTaskChange: (task: {id: string, title: string, completed: boolean}) => void}) {
+function TaskList({tasks, onTaskChange, onTaskRemove, onClearCompleted, onTaskComplete}: {
+  tasks: {id: string, title: string, completed: boolean}[],
+  onTaskChange: (task: {id: string, title: string, completed: boolean}) => void,
+  onTaskRemove: (taskId: string) => void,
+  onClearCompleted: () => void,
+  onTaskComplete: (taskId: string) => void}) {
 
   return (
     <>
       <ul className="flex flex-col rounded-lg bg-white shadow-lg shadow-indigo-500/25">
         <div className="flex px-2 py-2 justify-between text-gray-400 border-b border-b-gray-200 mx-2">
-          <div>items</div>
+          <div>items {tasks.length}</div>
           <div>filtros</div>
-          <div>clear completed</div>
+          <div> <span className="cursor-pointer hover:text-red-500" onClick={onClearCompleted}>clear completed</span> </div>
         </div>
         {tasks.map((task) => (
           <Task 
             key={task.id}
             task={task}
             onTaskChange={onTaskChange}
+            onTaskRemove={onTaskRemove}
+            onTaskComplete={onTaskComplete}
           />
         ))}
       </ul>
